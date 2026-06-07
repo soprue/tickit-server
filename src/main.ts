1786404980f35da8757e0a74d83dfb19931b7048
Gahee Kim <62260343+soprue@users.noreply.github.com>
2026-06-07 21:14:23 +0900

    chore: prepare railway deployment

--- src/main.ts
+++ src/main.ts
@@ -73,7 +73,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap().catch((err) => {
   console.error(err);
